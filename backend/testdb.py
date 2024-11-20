a0b7478fa41339c3120c4b0a21eda9a70bc5a8ff
NickKornienko <nickkornienko@outlook.com>
2024-11-19 20:07:38 -0800

    added backend db skeletion

---
+++ backend/testdb.py
@@ -0,0 +1,11 @@
+from app import app, get_conversation, handle_request
+from db.db import db
+
+# Ensure the app context is available
+with app.app_context():
+    # Call the add_conversation function
+    result = handle_request("Hello")
+    print(result)
+
+    result = get_conversation(result["id"])
+    print(result)
