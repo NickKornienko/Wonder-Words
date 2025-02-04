8f3ac6e0ab2a0c134d91ebd261a4e73b69c3687f
NickKornienko <nickkornienko@outlook.com>
2025-02-04 13:04:59 -0800

    added handler and new story logic

--- backend/llm/llm.py
+++ backend/llm/llm.py
@@ -1,22 +1,64 @@
-from .openai_models import gpt_4o_mini
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
 
-llm_type = 1
+model = "gpt-4o-mini"
+
+load_dotenv()
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY"),
+)
 
 
-def llm(query):
-    def llm_type_2(query):
-        return f"Response from LLM type 2 for query: {query}"
+def handler(query):
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": (
+                    "You are the handler for a storytelling AI that can generate children's stories based on a given prompt."
+                    "You should take the user input and decide what to do with it by returning the appropriate code, which is the inter only. Ex. 0, 1, 2, 3, 4, 5."
+                    "If the user asks for something unrelated to telling a story, respond with code 0."
+                    "If the user asks for something related to a story but violates safety rules, respond with code 1."
+                    "If the user asks for a new story, respond with code 2."
+                    "If the user asks for an addition to an existing story, respond with code 3."
+                ),
+            },
+            {
+                "role": "user",
+                "content": query,
+            }
+        ],
+        model=model,
+    )
 
-    def llm_type_3(query):
-        return f"Response from LLM type 3 for query: {query}"
+    return chat_completion.choices[0].message.content
 
-    switch = {
-        1: gpt_4o_mini,
-        2: llm_type_2,
-        3: llm_type_3,
-    }
 
-    selected_llm = switch.get(llm_type)
-    response = selected_llm(query)
+def new_story_generator(query):
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": (
+                    "You are the writer for a storytelling AI that can generate children's stories based on a given prompt."
+                    "You should take the user unput and generate a new story based on it."
+                    "The story should be appropriate for children and should be creative and engaging."
+                    "You should return the story as the output."
+                    "Only return the story and nothing else, include metacommentary 'such as sure here is a story or here is a story about'."
+                    "Limit the story to 100 words."
+                ),
+            },
+            {
+                "role": "user",
+                "content": query,
+            }
+        ],
+        model=model,
+    )
 
-    return response
+    return chat_completion.choices[0].message.content
+
+
+def add_to_story(query):
+    return 0
