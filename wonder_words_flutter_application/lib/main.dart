a745d97c40bba03b7873fbcb72aa5ad1e95abbe3
Alexis-Az <guoxuyang@Xuyangs-MBP.attlocal.net>
2025-03-03 14:49:16 -0800

    formatting model prompting

--- wonder_words_flutter_application/lib/main.dart
+++ wonder_words_flutter_application/lib/main.dart
@@ -5,17 +5,6 @@ import 'dart:convert';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:wonder_words_flutter_application/storyDetailsForm.dart';
 
-Future<String> _loadApiKeyFromConfigFile(String configFileName) async {
-  try {
-    // using rootBundle to access the config file
-    final content = await rootBundle.loadString('tokens/hf_token.json');
-    final jsonObject = jsonDecode(content);
-    return jsonObject["hf_token"];
-  } catch (e) {
-    throw Exception('Error reading API key from file: $e');
-  }
-}
-
 void main() async {
   runApp(StoryDetailsForm());
 
