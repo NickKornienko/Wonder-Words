422424e4b9723534b2f98218e88f31f81cf4febb
Joash <joash.muganda@gmail.com>
2025-03-27 18:29:53 -0700

    Update .gitignore to exclude sensitive files and remove sensitive files from git

--- wonder_words_flutter_application/lib/config/api_keys.dart
+++
@@ -1,6 +0,0 @@
-class ApiKeys {
-  // Google Cloud Text-to-Speech API key
-  // Get your API key from the Google Cloud Console: https://console.cloud.google.com/apis/credentials
-  static const String googleCloudApiKey =
-      'AIzaSyAulu6iFVdGXPXpOwo6RaMHObAgXgRobIs';
-}
