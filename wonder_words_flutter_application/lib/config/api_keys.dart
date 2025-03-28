ddf5ee36589b73d2c982ee845d31fbc81fffe53f
Joash <joash.muganda@gmail.com>
2025-03-27 18:22:14 -0700

    Fix .gitignore to include Flutter lib directory and add missing Flutter files

---
+++ wonder_words_flutter_application/lib/config/api_keys.dart
@@ -0,0 +1,6 @@
+class ApiKeys {
+  // Google Cloud Text-to-Speech API key
+  // Get your API key from the Google Cloud Console: https://console.cloud.google.com/apis/credentials
+  static const String googleCloudApiKey =
+      'AIzaSyAulu6iFVdGXPXpOwo6RaMHObAgXgRobIs';
+}
